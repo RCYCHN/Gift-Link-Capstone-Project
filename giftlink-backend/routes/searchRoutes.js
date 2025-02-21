6f5a0d838e86030f93d66c790d307ff84bfac129
Ricky Chun <rickychun39@gmail.com>
2025-02-21 06:52:32 +0100

    Update searchRoutes.js jshint esversion: 8

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 8 */
+
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
@@ -14,8 +16,9 @@ router.get('/', async (req, res, next) => {
         let query = {};
 
         // Add the name filter to the query if the name parameter is not empty
-        req.query.name>req.query.name && req.query.name.trim() !== ''
+        if (req.query.name && req.query.name.trim() !== '') {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
+        }
 
         // Task 3: Add other filters to the query
         if (req.query.category) {
@@ -29,7 +32,7 @@ router.get('/', async (req, res, next) => {
         }
 
         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        const gifts = await collection.find(query).toArray()
+        const gifts = await collection.find(query).toArray();
 
         res.json(gifts);
     } catch (e) {
